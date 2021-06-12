66ace10b3d13c727bbff3a6f665948f32004796a
Rajat Chand <rajatc@uw.edu>
2021-06-12 14:22:10 -0700

    working version of dmd

--- src/dmd.py
+++ src/dmd.py
@@ -1,44 +1,101 @@
 import numpy as np
 
 class DMD:
-    def __init__(self, dt, projected = True, svd_rank = None):
+    def __init__(self, projected=True):
+        self.snapshots = None
         self.projected = projected
-        self.dt = dt
         self.eigenvalues = None
         self.eigenvectors = None
+        self.omegas = None
         self.modes = None
+        self.coefficients = None
+        self.dt = None
 
-    def fit(self, x, rank):
+    def solve(self, x, dt):
+        """
+        :param x:
+        :param dt:
+        :param rank:
+        :return:
+        """
+        x = DMD.create_tallskinny(x)
+        self.snapshots = x
+        self.dt = dt
         x1 = x[:, :-1]
         x2 = x[:, 1:]
+
         u, s, v = np.linalg.svd(x1, full_matrices=False)
         v = v.T
-
-        u = u[:, :rank]
-        v = v[:, :rank]
-        s = s[:rank]
+        #u = u[:, :rank]
+        #v = v[:, :rank]
+        #s = s[:rank]
 
         a_tilde = np.linalg.multi_dot([u.T, x2, v]) * np.reciprocal(s)
         eigenvalues, eigenvectors = np.linalg.eig(a_tilde)
 
+        self.eigenvalues = eigenvalues
+        self.omegas = np.log(self.eigenvalues)/self.dt
+        self.eigenvectors = eigenvectors
         if self.projected:
-            v11 = u.dot(W)
+            self.modes = u.dot(eigenvectors)
         else:
-            v11 = ((Y.dot(V) *np.reciprocal(Sigma)).dot(W))
+            self.modes = ((x2.dot(v) * np.reciprocal(s)).dot(eigenvectors))
 
+        self.compute_coefficients(False)
 
+    def compute_coefficients(self, exact = False):
+        """
 
-    def compute_amplitudes(self):
+        :param exact:
+        :return:
+        """
+        if exact:
+            self.coefficients = np.linalg.solve(self.modes, self.snapshots[:, 0])
+        else:
+            self.coefficients = np.linalg.lstsq(self.modes, self.snapshots[:, 0])[0]
 
+    def evolve(self, time_array=None, mask=None):
+        """
+        Evolves the dynamical system in time according to the
+        eigendecomposition of A.
+        :param time_array: array of time for which to approximate state.
+            If None, then approximate state at the time points
+            where snapshots are taken
+        :return: array where each column is approximated state at time t
+        """
 
+        masked_eigenvalues = self.eigenvalues
+        masked_modes = self.modes
+        if mask is not None:
+            masked_eigenvalues[~mask] = 0
+            masked_modes[:, ~mask] = 0
 
+        if time_array is None:
+            num_snapshots = self.snapshots.shape[1]
+            tbase = np.outer(masked_eigenvalues, np.ones(num_snapshots))
+            tpower = np.arange(start=0, stop=num_snapshots)
+        else:
+            tbase = np.outer(masked_eigenvalues, np.ones(time_array.size))
+            tpow = time_array/self.dt
 
+        y1 = np.power(tbase, tpower)
+        y2 = y1 * self.coefficients[:, None]
+        return masked_modes.dot(y2)
 
+    def create_mask(self, threshold = 0.1):
+        return np.abs(self.omegas) < threshold
 
+    @staticmethod
+    def create_tallskinny(x):
+        """
+        :param x:
+        :return:
+        """
+        cols = x.shape[0]
+        rows = int(x.size/x.shape[0])
+        data_matrix = np.zeros(shape=(rows, cols))
 
+        for i in range(cols):
+            data_matrix[:, i] = x[i].flatten(order = 'C')
 
-
-
-
-
-    
+        return data_matrix
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,12 @@
 import cv2
 import numpy as np
 
-class Video:
+class VideoIO:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.cap = None
-
-    def __enter__(self):
         self.cap = cv2.VideoCapture(self.file_path)
-        return self.cap
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def teardown(self):
         if self.cap:
             self.cap.release()
 
@@ -18,21 +14,23 @@ class Video:
         self.cap.set(1, start)
 
         count = 0
+        frames_list = []
         while count < total_frames:
             ret, frame = self.cap.read()
             if not ret:
                 raise Exception("frame cannot be read!")
             if gray:
-                frame = Video.convert_grayscale(frame)
-            frame.
-
+                frame = VideoIO.convert_grayscale(frame)
+                frame = cv2.pyrDown(frame)
+                frame = cv2.pyrDown(frame)
+            frames_list.append(frame)
             count = count + 1
+
+        return np.asarray(frames_list)
+
+    def get_fps(self):
+        return self.cap.get(cv2.CAP_PROP_FPS)
 
     @staticmethod
     def convert_grayscale(frame):
         return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-
-#creates row major tall skinny vector from a list of numpy nd array
-def create_tall_skinny(X):
-    return X.flatten(order = 'C')
